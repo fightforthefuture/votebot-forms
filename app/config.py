5d6a4d1ae8117696706826539a5f7444327a8728
Josh Levinger <josh@levinger.net>
2016-08-08 19:27:54 -0700

    RQ FURL

--- app/config.py
+++ app/config.py
@@ -2,6 +2,7 @@ import os
 
 DEBUG = os.environ.get('DEBUG', True)
 REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379/')
+RQ_REDIS_URL = REDIS_URL
 
 SMARTY_STREETS_AUTH_ID = os.environ.get('SMARTY_STREETS_AUTH_ID')
 SMARTY_STREETS_AUTH_TOKEN = os.environ.get('SMARTY_STREETS_AUTH_TOKEN')
