62413028bd7f613cf279a8dc53338ebd8a2986dc
Josh Levinger <josh@levinger.net>
2016-08-11 12:26:23 -0700

    notes

--- app/jobs.py
+++ app/jobs.py
@@ -9,8 +9,14 @@ def submit_form(form, user, callback_url):
 
     if form.__class__.__name__ is 'VoteDotOrg':
         # create new job for pdf check
-        # delay a few seconds?
+        # TODO delay a few seconds?
+        # TODO retry automatically if failed?
         get_pdf.queue(form, user, callback_url)
+
+
+    # TODO
+    # log form.browser final state, so we can determine sucess or error strings
+    # send to votebot-api database, for simplicity
 
     if callback_url:
         requests.post(callback_url, jsonify(status))
